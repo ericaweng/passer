c1af1e0ae9b34d65ae9891ca7b5ee43a2d11f2df
EricaWeng <ericaxw99@gmail.com>
2023-03-19 20:51:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# passer
+
+erica's blog
+
+![cute chibi sparrow](./public/bird.png)
+
+excellent blog template borrowed from [morethan-log](https://github.com/morethanmin/morethan-log).
---
+++ site.config.js
@@ -0,0 +1,64 @@
+const CONFIG = {
+  profile: {
+    name: "erica weng",
+    image: "/bird.png",
+    bio: "I like birds",
+    github: "",
+    linkedin: "",
+    email: "",
+    instagram: "",
+    role: "",
+  },
+  projects: '',
+  // blog setting (required)
+  blog: {
+    title: "passer-lime",
+    description: "erica's sparrow feed",
+    theme: "auto", // ['light', 'dark', 'auto']
+  },
+
+  // CONFIG configration (required)
+  link: "https://passer-lime.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ["Blog", "Website", "Notion"],
+  },
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: false,
+    config: {
+      repo: "ericaweng/passer",
+      "issue-term": "og:title",
+      label: "",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+module.exports = CONFIG
